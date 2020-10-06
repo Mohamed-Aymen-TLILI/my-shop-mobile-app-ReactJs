1b13235a46975c22e2437a24349fc58a991ae951
libra1919 <aymentli@gmail.com>
2020-10-06 11:30:48 +0200

    Updates

---
+++ precache-manifest.87689e256ab7e14d811b9b1af41f78b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bee484d3491c35d89e2d9858e39748",
+    "url": "/products/index.html"
+  },
+  {
+    "revision": "4ec93c96ff952cb5fa80",
+    "url": "/products/static/css/main.30c0dc76.chunk.css"
+  },
+  {
+    "revision": "2849e1a73fb47aca50eb",
+    "url": "/products/static/js/2.ddafa4c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/products/static/js/2.ddafa4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec93c96ff952cb5fa80",
+    "url": "/products/static/js/main.632a171e.chunk.js"
+  },
+  {
+    "revision": "03114e70e25b90f27816",
+    "url": "/products/static/js/runtime-main.8e831060.js"
+  }
+]);
